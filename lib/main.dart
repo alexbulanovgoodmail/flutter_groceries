2a842b7c3c00de440ff2ba7d585810a404deae2b
Александр Буланов <alexbulanovgoodmail@gmail.com>
2026-03-03 17:23:25 +0300

    Настройка проекта: модели и мок данные

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter Groceries',
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+      ),
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello, Groceries!',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
         ),
       ),
     );
